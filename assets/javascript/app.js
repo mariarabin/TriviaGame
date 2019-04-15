13b49d2d9daf19e03d80d1290923190bbdffc21b
Maria <lrabin@gmail.com>
2019-04-15 11:08:26 -0400

    draft3

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,8 +2,63 @@
 $(document).ready(function () {
     //start of the game - start timer
     alert("Let's get R-E-A-D-Y👾!!!");
-    //======VARIABLES========
+    $('form[name="quiz"]').hide();
+    $("#ans").hide();
+
+
+
+
+    $("#start").on('click', function () {
+        $('form[name="quiz"]').show();
+        $("#ans").hide();
+        $("#start").hide();
+        $("#cor").text("TBD");
+        $("#inc").text("TBD");
+        timeOut();
+        timerStarts();
+
+    });
+
+
 });
+
+
+
+//======VARIABLES========
+
+var timeout = 0;
+var time = 5;
+var score = 0;
+var inc = 0;
+var numQs = 2;
+var correctAns = [];
+
+
+
+//===MAIN PROCESS=========================
+
+function timeOut() {
+    setTimeout(function () { alert("Session Expired"); }, 60000);
+};
+
+function timerStarts() {
+    intervalId = setInterval(count, 1000)
+};
+
+function count() {
+    //time = 30;
+    time--;
+    if (time === 0) {
+        alert("Time is up");
+        clearInterval(intervalId);
+    };
+    $("#timing").text(time);
+};
+
+
+
+
+
 
 $("#submit").on('click', function () {
     alert("Submitting");
@@ -13,25 +68,22 @@ $("#submit").on('click', function () {
 
 function onSubmit() {
     var score = 0;
-    //var numQs = 5;
+    var numQs = 2;
     var correctAns = ['a', 'b'];
+    var inc = 0;
 
     //var q1 = document.forms['quiz']['q1'].value;
-    //var q2 = document.forms['quiz']['q2'].value;
-
-
-    // for (var i = 1; i <= numQs; i++) {
-    // if (eval('q' + i) == ' ') {
-    // alert("You missed question" + i);
-    // }
-    // }
 
     for (var i = 0; i < correctAns.length; i++) {
         var qx = document.forms["quiz"]["q" + (i + 1)].value;
-        alert("i:" + i + " qx:" + qx + " cAns:" + correctAns[i]);
+        //alert("i:" + i + " qx:" + qx + " cAns:" + correctAns[i]);
         if ((!!correctAns[i]) && (qx) == correctAns[i]) {
             score++;
             $("#cor").text(score);
+            $("#inc").text(2 - score);
+        }
+        else {
+            $("#inc").text(2 - score);
         }
     }
     return false;
