9130f3aa7d7f696f8353d29b7d936a92f68c11d3
Maria <lrabin@gmail.com>
2019-04-17 23:19:29 -0400

    updated the timeOut

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,19 @@
 // Page loads as a start of the game
 $(document).ready(function () {
     $('form[name="quiz"]').hide();
+
+
     //start of the game - start timer
     //alert("Let's get R-E-A-D-Y👾!!!");
-    timeOut();
+
     $("#yourAns").hide();
     $("#ans").hide();
     $("#answers").hide();
 
 
 
-
     $("#start").on('click', function () {
+        timeOut();
         $('form[name="quiz"]').show();
         $("#ans").hide();
         $("#answers").hide();
@@ -21,16 +23,16 @@ $(document).ready(function () {
         timerStarts();
     });
 
+
     $("#submit").on('click', function () {
         alert("Submitting");
         onSubmit();
-        //$("input[type=radio]").disabled = true;
         $('form[name="quiz"]').attr("disabled", true);
         //$("quiz :input").attr("disabled", true);
+        //$("input[type=radio]").disabled = true;
         typeAns();
 
     })
-
 
 });
 
@@ -43,7 +45,8 @@ var time = 60;
 var score = 0;
 var inc = 0;
 var numQs = 10;
-var correctAns = [];
+var correctAns = ['a', 'a', 'b', 'c', 'c', 'a', 'b', 'a', 'b', 'b'];
+
 
 
 
@@ -51,18 +54,28 @@ var correctAns = [];
 
 function timeOut() {
     setTimeout(function () { alert("Session Expired"); }, 120000);
+    //disableRadioButtons();
+    //startPage();
+
 
 };
+
+function startPage() {
+    $("#blankPage").load(blankpage.html);
+};
+
 
 function timerStarts() {
     intervalId = setInterval(count, 1000)
 };
+
 
 function count() {
     time--;
     if (time == -1) {
         alert("Time is up");
         clearInterval(intervalId);
+        onSubmit();
         return;
     };
     $("#timing").text(time);
@@ -70,20 +83,37 @@ function count() {
 
 
 
-
-
+function disableRadioButtons() {
+    $("#start").hide();
+    for (var i = 0; i < correctAns.length; i++) {
+        for (var j = 1; j < 4; j++) {
+            //document.getElementById("q" + (i + 1) + "" + j).disabled = true;
+            const radioBtnId = ("#q" + (i + 1) + "" + j);
+            $(radioBtnId)[0].disabled = true;
+            //console.log(Object.getOwnPropertyNames($(radioBtnId)));
+            //console.log(Object.getOwnPropertyNames($(radioBtnId)[0]));
+            //console.log($(radioBtnId)[0]);
+            //$(radioBtnId)[0].disabled = true;
+            //console.log($(radioBtnId)[0].disabled);
+            //console.log(Object.getOwnPropertyNames($(radioBtnId)));
+        }
+    }
+}
 
 
 function onSubmit() {
     var score = 0;
     var numQs = 10;
-    var correctAns = ['a', 'a', 'b', 'c', 'c', 'a', 'b', 'a', 'b', 'b'];
+
     var inc = 0;
     var userAns = [];
 
     //var q1 = document.forms['quiz']['q1'].value;
+    //$("input[type=radio]").enable = true;
+    //$('input').prop('disabled', true);
 
     for (var i = 0; i < correctAns.length; i++) {
+        disableRadioButtons();
         var qx = document.forms["quiz"]["q" + (i + 1)].value;
         //alert(qx);
         $("#yourAnswers").html(qx);
